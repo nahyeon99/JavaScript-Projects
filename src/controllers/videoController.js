33cc4db7c5c8bfe94784cb7c27144c5a3446c174
NaHyeon <nahyeonee99@gmail.com>
2021-12-21 07:41:42 +0900

    [wetube] Pug with fake data

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,41 +1,67 @@
+let videos = [
+  {
+    title: "video #1",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minute ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "video #2",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minute ago",
+    views: 1,
+    id: 2,
+  },
+  {
+    title: "video #3",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minute ago",
+    views: 1,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "video #1",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minute ago",
-      views: 1,
-      id: 1,
-    },
-    {
-      title: "video #2",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minute ago",
-      views: 1,
-      id: 2,
-    },
-    {
-      title: "video #3",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minute ago",
-      views: 1,
-      id: 3,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const see = (req, res) => {
-  res.render("watch", { pageTitle: "Watch" });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+
+  res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
 
-export const search = (req, res) => res.send("search");
-
-export const editVideo = (req, res) => {
-  res.render("edit", { pageTitle: "edit videos" });
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Editing ${video.title}`, video });
 };
-export const deleteVideo = (req, res) => res.send("deleteVideo");
-export const upload = (req, res) => res.send("upload");
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  console.log(title);
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  res.render("upload", { pageTitle: "upload" });
+};
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "2 minutes age",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,16 +1,19 @@
 import express from "express";
 import {
-  see,
-  editVideo,
+  edit,
   deleteVideo,
   upload,
+  watch,
+  getEdit,
+  postEdit,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", editVideo);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
